461d33310bf86c2c2c6c8c794a808078ecdf3c99
Rodriguez <771974@cognizant.com>
2021-03-20 11:00:54 -0500

    RED post test adding movie

--- src/test/java/com/galvanize/GMDB/integration/MovieControllerTest.java
+++ src/test/java/com/galvanize/GMDB/integration/MovieControllerTest.java
@@ -4,27 +4,48 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
 public class MovieControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
     @Test
     public void getMovieTest() throws Exception {
         RequestBuilder go = get("/movies");
         mockMvc.perform(go)
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(0)))
+                .andExpect(jsonPath("$", hasSize(0)))
+                .andDo(print());
+    }
+
+    @Test
+    public void postMovieWhenReleasedTest() throws Exception {
+        RequestBuilder post = post("/movies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{}");
+
+        mockMvc.perform(post)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("message").value("Movie has been created."))
+                .andDo(print());
+
+        RequestBuilder get = get("/movies");
+        mockMvc.perform(get)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
                 .andDo(print());
 
     }
