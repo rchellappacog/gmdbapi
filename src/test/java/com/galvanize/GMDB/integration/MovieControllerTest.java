e0653a28c3e766971c67c3dd7d1d3026ebc3c0a9
rchellappacog <Ravindran.Chellappa@cognizant.com>
2021-03-20 11:32:27 -0400

    Red T Acceptance Scenario 1

---
+++ src/test/java/com/galvanize/GMDB/integration/MovieControllerTest.java
@@ -0,0 +1,25 @@
+package com.galvanize.GMDB.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class MovieControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void getMovieTest() throws Exception {
+        RequestBuilder go = get("/movies");
+        mockMvc.perform(go)
+                .andExpect(status().isOk());
+    }
+}
